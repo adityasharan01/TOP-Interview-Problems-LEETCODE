aae6e852f091444778c7c5a8ce9a91dcf75a87be
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2022-02-24 02:36:39 +0530

    Update 01Uber pool.java

--- Medium/01Uber pool.java
+++ Medium/01Uber pool.java
@@ -1,4 +1,6 @@
-You are given a two-dimensional integer list requested_trips containing [start_x, end_x, num_passengers], and an integer capacity. Each requested trip asks to pick up num_passengers passenger at start_x and drop them off at end_x. You also have a car with the given capacity, and start at x = 0.
+You are given a two-dimensional integer list requested_trips containing [start_x, end_x, num_passengers], and an integer capacity.
+    Each requested trip asks to pick up num_passengers passenger at start_x and drop them off at end_x. You also have a car with the given capacity, 
+and start at x = 0.
 
 Given that you'd like to pick up every passenger and can only move right, return whether you can pick up and drop off everyone.
 
